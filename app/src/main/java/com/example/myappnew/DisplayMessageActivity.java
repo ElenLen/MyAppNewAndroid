07876e858b8309bb2d6a61710a742262c8658203
ElenLen <relax-pl@yandex.ru>
2021-06-27 22:48:28 +0500

    first commit

--- app/src/main/java/com/example/myappnew/DisplayMessageActivity.java
+++ app/src/main/java/com/example/myappnew/DisplayMessageActivity.java
@@ -11,13 +11,12 @@ public class DisplayMessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
-        // Get the Intent that started this activity and extract the string
 
+        // Получите намерение, с которого началось это действие, и извлеките строку
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
 
-        // Capture the layout's TextView and set the string as its text
-
+        // Захватите текстовое представление макета и установите строку в качестве его текста
         TextView textView = findViewById(R.id.textView);
         textView.setText(message);
     }
--- app/src/main/java/com/example/myappnew/MainActivity.java
+++ app/src/main/java/com/example/myappnew/MainActivity.java
@@ -7,13 +7,14 @@ import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    public static final String EXTRA_MESSAGE = "com.example.myappnew.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    //Вызывается, когда пользователь нажимает кнопку Отправить
     public void sendMessage(View view) {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText) findViewById(R.id.editText);
